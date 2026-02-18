c14302eaf7459840a5ec9cd20b3a21be7caeeda3
jessicapinili <jessicampinili@gmail.com>
2026-02-18 12:28:13 +1000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  define: {
-    // This allows the build process to handle process.env references
-    // common in Gemini API code samples while keeping them compatible with static hosting.
-    'process.env': process.env
-  }
-});
+  base: '/',
+})
